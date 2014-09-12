b4118e8338323b9de0aad968b7bd3eccd5e7402b
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-09-12 17:31:55 -0300

    Add s to compat

--- gulib/compat.py
+++ gulib/compat.py
@@ -23,14 +23,16 @@ except NameError:
         return any("__call__" in klass.__dict__ for klass in type(obj).__mro__)
 
 if PYTHON3:
-    def b(s): return s.encode("latin-1")
+    def b(s): return s.encode("utf-8")
     def u(s): return s
+    def s(s): return s.decode("utf-8")
     ord2 = lambda x: x
     chr2 = lambda x: bytes([x])
     bytes3 = bytes
 else:
     def b(s): return s
     def u(s): return unicode(s.replace(r'\\', r'\\\\'), "unicode_escape")
+    def s(s): return str(s)
     ord2 = ord
     chr2 = chr
     bytes3 = lambda x: bytes().join([chr(c) for c in x])
